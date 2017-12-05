a2eb6d0c1ce726f0b6d0091b554d6f7cb350a666
Lokhin Wu <lokhin.wu@live.com>
2017-12-05 14:12:25 -0500

    organize folder structure

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import TabView from './TabView';
+import TabView from './components/TabView';
 
 export default class App extends React.Component {
   constructor(props) {
