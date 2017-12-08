5dfca609c5168a9eef719edec3843a02e0dde83a
Lokhin Wu <lokhin.wu@live.com>
2017-12-08 13:07:49 -0500

    todo: constructor card

--- components/ConstructorCard.js
+++ components/ConstructorCard.js
@@ -5,7 +5,8 @@ import {
   View,
   ActivityIndicator,
   ListView,
-  Image
+  Image,
+  Dimensions
 } from 'react-native';
 
 export default class ConstructorCard extends React.Component {
@@ -16,13 +17,13 @@ export default class ConstructorCard extends React.Component {
     };
   }
   componentDidMount() {
-    return fetch('http://ergast.com/api/f1/constructors/' + this.props.navigation.state.params.constructorId + '.json').then((response) => response.json()).then((responseJson) => {
+    return fetch('https://bsbbycpjma.execute-api.eu-central-1.amazonaws.com/latest/f1/constructor/' + this.props.navigation.state.params.constructorId + '/details').then((response) => response.json()).then((responseJson) => {
       let ds = new ListView.DataSource({
         rowHasChanged: (r1, r2) => r1 !== r2
       });
       this.setState({
         isLoading: false,
-        constructorInfo: ds.cloneWithRows(responseJson.MRData.ConstructorTable.Constructors)
+        constructorInfo: ds.cloneWithRows(responseJson)
       }, function() {});
     }).catch((error) => {
       console.error(error);
@@ -39,18 +40,22 @@ export default class ConstructorCard extends React.Component {
         </View>
       );
     }
-    return (<ListView dataSource={this.state.constructorInfo} renderRow={(rowData) => <View style={styles.driver}>
+    return ( <View style={styles.driver}>
       <View style={{
         flex: 1,
         flexDirection: 'row'
       }}>
         <Image source={{
-          uri: 'https://s3-eu-west-1.amazonaws.com/f1-storage/Cars/' + rowData.constructorId + '.jpg'
+          uri: 'https://s3-eu-west-1.amazonaws.com/f1-storage/Cars/' + this.props.navigation.state.params.constructorId + '.jpg'
         }} style={styles.car}></Image>
       </View>
-    </View>}/>);
+      <Text>{this.state.constructorInfo.races}</Text>
+
+    </View>);
   }
 }
+
+const { width } = Dimensions.get('window')
 
 const styles = StyleSheet.create({
   container: {
@@ -60,7 +65,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   car: {
-    width: 200,
-    height: 50
+    width: width,
+    height: ((width) / 3.36956522)
   }
 });
